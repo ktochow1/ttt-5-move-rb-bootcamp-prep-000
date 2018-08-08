d8e68b5fde33592897b9036a5047365d3e73d568
ktochow1 <ktochow1@binghamton.edu>
2018-08-08 20:01:44 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,7 +11,7 @@ def input_to_index(user_input)
 end
 
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-def move(board, index, "X" )
+def move(board, index, value="X" )
   #array[index] = value
 end
 move(board, 2)
